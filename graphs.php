27567dd44926ebdaa5cd5a3577ba1a3e174fa317
elazar <tobias382@gmail.com>
2010-05-11 20:54:39 -0500

    Modified use of terminology in the graph generator to be consistent with that of the presentation and changed its output format from JPEG to PNG

--- graphs.php
+++ graphs.php
@@ -14,14 +14,14 @@ fgetcsv($log);
 
 $data = array(
     'memory' => array(),
-    'rps' => array()
+    'eps' => array()
 );
 
 while ($line = fgetcsv($log)) {
-    list ($elements, $file, $time, $rps, $memory) = $line;
+    list ($elements, $file, $time, $eps, $memory) = $line;
     list ($component, $test) = explode('-', str_replace('.php', '', $file));
     $data['memory'][$component][$test][$elements] = ceil($memory / 1024); // convert to KB
-    $data['rps'][$component][$test][$elements] = $rps;
+    $data['eps'][$component][$test][$elements] = $eps;
 }
 
 fclose($log);
@@ -31,12 +31,16 @@ foreach ($data as $measurement => $components) {
         echo $component, ' - ', $measurement, PHP_EOL;
 
         $graph = new ezcGraphBarChart;
+        $graph->driver = new ezcGraphGdDriver;
+        $graph->options->font = '/usr/share/fonts/truetype/msttcorefonts/arial.ttf';
+        $graph->driver->options->supersampling = 1;
+        $graph->driver->options->imageFormat = IMG_PNG;
         $graph->xAxis->label = 'Elements';
-        $graph->yAxis->label = ($measurement == 'memory') ? 'Memory (KB)' : 'Requests / Second';
+        $graph->yAxis->label = ($measurement == 'memory') ? 'Memory (KB)' : 'Executions / Second';
         $graph->title = $component . ' - ' . $measurement;
         foreach ($tests as $test => $elements) {
             $graph->data[$test] = new ezcGraphArrayDataSet($elements);
         }
-        $graph->render(400, 225, 'results/' . $component . '_' . $measurement . '.svg');
+        $graph->render(400, 225, 'results/' . $component . '_' . $measurement . '.png');
     }
 }
